2510b86a2e10baf38ce180e56924a31f11cf543d
Himesh Kurmi <rajendrakurmi729@gmail.com>
2026-02-20 16:45:00 +0530

    [Himesh] : added quantity Measurement app file

--- src/test/java/quantitymeasurementapp/TestFeetEquality.java
+++ src/test/java/quantitymeasurementapp/TestFeetEquality.java
@@ -1,52 +1,47 @@
 package quantitymeasurementapp;
 
+import static org.junit.jupiter.api.Assertions.*;
 
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.Scanner;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import quantitymeasurementapp.FeetEquality;
+import quantitymeasurementapp.QuantityMeasurement.FeetEquality;
 
 public class TestFeetEquality {
-	FeetEquality feet1;
-	FeetEquality feet2;
+
+    FeetEquality feet1;
+    FeetEquality feet2;
+
     @Test
     public void testEquals() {
-    	feet1 = new FeetEquality(1);
-    	feet2 = new FeetEquality(1);
-    	assertTrue(feet1.equals(feet2));
+        feet1 = new FeetEquality(1);
+        feet2 = new FeetEquality(1);
+        assertEquals(feet1, feet2);
     }
-    
+
     @Test
     public void testDifferent() {
-    	feet1 = new FeetEquality(1);
-    	feet2 = new FeetEquality(2);
-    	assertTrue(!feet1.equals(feet2));
+        feet1 = new FeetEquality(1);
+        feet2 = new FeetEquality(2);
+        assertNotEquals(feet1, feet2);
     }
-    
+
     @Test
     public void testNull() {
-    	feet1 = new FeetEquality(1);
-    	feet2 = null;
-    	assertTrue(!feet1.equals(feet2));
+        feet1 = new FeetEquality(1);
+        feet2 = null;
+        assertNotEquals(feet1, feet2);
     }
-    
+
     @Test
-    public void testSameRefferencet() {
-    	feet1 = new FeetEquality(1);
-    	
-    	assertTrue(feet1.equals(feet1));
+    public void testSameReference() {
+        feet1 = new FeetEquality(1);
+        assertEquals(feet1, feet1);
     }
-    
+
     @Test
     public void testEmptyVariable() {
-    	feet1 = new FeetEquality(1);
-    	feet2 = new FeetEquality();
-    	assertTrue(!feet1.equals(feet2));
+        feet1 = new FeetEquality(1);
+        feet2 = new FeetEquality(); // default value = 0.0
+        assertNotEquals(feet1, feet2);
     }
-    
 }
