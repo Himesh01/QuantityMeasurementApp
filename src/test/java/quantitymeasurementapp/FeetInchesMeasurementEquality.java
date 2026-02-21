d0c4ef2eb775cdc61edf5581789e779a69c543cb
Himesh Kurmi <rajendrakurmi729@gmail.com>
2026-02-22 00:33:27 +0530

    [Himesh] : added Feet and Inches measurement equality file

---
+++ src/test/java/quantitymeasurementapp/FeetInchesMeasurementEquality.java
@@ -0,0 +1,89 @@
+package quantitymeasurementapp;
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import quantitymeasurementapp.QuantityMeasurement;
+import quantitymeasurementapp.QuantityMeasurement.FeetEquality;
+import quantitymeasurementapp.QuantityMeasurement.Inches;
+
+public class FeetInchesMeasurementEquality {
+		
+		QuantityMeasurement.FeetEquality feet1;
+		QuantityMeasurement.FeetEquality feet2;
+		QuantityMeasurement.Inches inche1;
+		QuantityMeasurement.Inches inche2;
+		
+	    @Test
+	    public void testFeetEquals() {
+	    	feet1 = new FeetEquality(1);
+	    	feet2 = new FeetEquality(1);
+	    	assertTrue(feet1.equals(feet2));
+	    }
+	    
+	    @Test
+	    public void testFeetDifferent() {
+	    	feet1 = new FeetEquality(1);
+	    	feet2 = new FeetEquality(2);
+	    	assertTrue(!feet1.equals(feet2));
+	    }
+	    
+	    @Test
+	    public void testFeetNull() {
+	    	feet1 = new FeetEquality(1);
+	    	feet2 = null;
+	    	assertTrue(!feet1.equals(feet2));
+	    }
+	    
+	    @Test
+	    public void testFeetSameRefferencet() {
+	    	feet1 = new FeetEquality(1);
+	    	
+	    	assertTrue(feet1.equals(feet1));
+	    }
+	    
+	    @Test
+	    public void testFeetEmptyVariable() {
+	    	feet1 = new FeetEquality(1);
+	    	feet2 = new FeetEquality();
+	    	assertTrue(!feet1.equals(feet2));
+	    }
+	    
+	    @Test
+	    public void testIncheEmptyVariable() {
+	    	inche1 = new Inches(1);
+	    	inche2 = new Inches();
+	    	assertTrue(!inche1.equals(inche2));
+	    }
+	    
+	    @Test
+	    public void testIncheSameRefference() {
+	    	inche1 = new Inches(1);
+	    	
+	    	assertTrue(inche1.equals(inche1));
+	    }
+	
+	    @Test
+	    public void testIncheNull() {
+	    	inche1 = new Inches(1);
+	    	inche2 = null;
+	    	assertTrue(!inche1.equals(inche2));
+	    }
+	    
+	    @Test
+	    public void testIncheDifferent() {
+	    	inche1 = new Inches(1);
+	    	inche2 = new Inches(2);
+	    	assertTrue(!inche1.equals(inche2));
+	    }
+	    
+	    @Test
+	    public void testInchesEqual() {
+	    	inche1 = new Inches(1);
+	    	inche2 = new Inches(1);
+	    	assertTrue(inche1.equals(inche2));
+	    }
+
+}
