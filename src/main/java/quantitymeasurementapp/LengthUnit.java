227075677cbdbf617576b22ea8b17504ff7a0180
Himesh Kurmi <rajendrakurmi729@gmail.com>
2026-02-24 01:24:46 +0530

    [Himesh] : added Length class

--- src/main/java/quantitymeasurementapp/LengthUnit.java
+++ src/main/java/quantitymeasurementapp/LengthUnit.java
@@ -1,6 +1,6 @@
 package quantitymeasurementapp;
 
-public enum LengthUnit{
+public enum LengthUnit implements IMeasurable{
 	FEET(1.0),
     INCHES(1.0 / 12.0),
     YARD(3.0),
@@ -18,7 +18,14 @@ public enum LengthUnit{
 	public double convertToBaseUnit(double value) {
    	 return value*this.getConversionFactor();
     }
-	public double convertFromBaseUnit(double value) {
+    public double convertFromBaseUnit(double value) {
     	return (LengthUnit.FEET.getConversionFactor()*value)/this.getConversionFactor();
     }
+
+
+	@Override
+	public String getUnitName() {
+		// TODO Auto-generated method stub
+		return LengthUnit.this.name();
+	}
 }
