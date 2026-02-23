c9e11baed6c299089d4cbb234a1e426f6e8868ba
Himesh Kurmi <rajendrakurmi729@gmail.com>
2026-02-24 01:24:26 +0530

    [Himesh] : added InvalidUnitMeasurementException.java class

--- src/main/java/quantitymeasurementapp/InvalidUnitMeasurementException.java
+++ src/main/java/quantitymeasurementapp/InvalidUnitMeasurementException.java
@@ -3,6 +3,5 @@ package quantitymeasurementapp;
 public class InvalidUnitMeasurementException extends Exception {
    public InvalidUnitMeasurementException(String str) {
 	   super(str);
-	   
    }
 }
