1cbcec508b44c64b89ce7278520d653cbbcf8e76
Himesh Kurmi <rajendrakurmi729@gmail.com>
2026-03-14 15:49:57 +0530

    [Himesh] : added exception file

--- src/main/java/quantitymeasurementapp/reposistory/IQuantityMeasurementRepository.java
+++ src/main/java/quantitymeasurementapp/reposistory/IQuantityMeasurementRepository.java
@@ -1,9 +1,19 @@
 package quantitymeasurementapp.reposistory;
 
 import quantitymeasurementapp.model.QuantityMeasurementEntity;
+
+
+import java.sql.ResultSet;
 import java.util.*;
 
-public interface IQuantityMeasurementRepository {
+ public interface IQuantityMeasurementRepository {
       void save(QuantityMeasurementEntity enity);
       List<QuantityMeasurementEntity> getAllMeasurement();
+      List<QuantityMeasurementEntity> getMeasurementByOperation(String Operation);
+      List<QuantityMeasurementEntity> getMeasurementByType(String type);
+      int getTotalCount();
+      void deleteALl();
+      String getPoolStatistic();
+      void releaseResource();
+     
 }
