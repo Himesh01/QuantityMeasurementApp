4648a672495da9917aac14ab2fec2087469b731d
Himesh Kurmi <rajendrakurmi729@gmail.com>
2026-04-06 10:48:54 +0530

    [Himesh] : added QuantityMeasurementAppApplicationTests.java file

--- src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppApplicationTests.java
+++ src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppApplicationTests.java
@@ -1,12 +1,24 @@
 package com.apps.quantitymeasurement;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.apps.quantitymeasurement.quantity.Quantity;
+import com.apps.quantitymeasurement.unit.VolumeUnit;
 
 @SpringBootTest
 class QuantityMeasurementAppApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void getTested() {
+	   Quantity<VolumeUnit> v1 = new Quantity<>(2.0,VolumeUnit.LITRE);
+	   Quantity<VolumeUnit> v2 = new Quantity<>(2.0,VolumeUnit.LITRE);
+	   assertEquals(4.0,v1.add(v2).getValue());
 	}
 }
