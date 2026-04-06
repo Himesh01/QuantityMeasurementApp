6c4669afdbec2ea7fecc5e88c7365f8cc96e8d6f
Himesh Kurmi <rajendrakurmi729@gmail.com>
2026-04-06 10:59:03 +0530

    [Himesh] : added AuthService.java file

---
+++ src/main/java/com/apps/quantitymeasurement/service/AuthService.java
@@ -0,0 +1,44 @@
+package com.apps.quantitymeasurement.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.apps.quantitymeasurement.dto.AuthDtoRequest;
+import com.apps.quantitymeasurement.model.User;
+import com.apps.quantitymeasurement.repository.SqlDatabase;
+import com.apps.quantitymeasurement.security.JwtUtil;
+import com.apps.quantitymeasurement.security.UserDetail;
+
+@Service
+public class AuthService {
+	
+	@Autowired
+	private SqlDatabase repo;
+    
+	@Autowired
+	private UserDetail user;
+	
+	@Autowired
+	private PasswordEncoder encoder;
+	
+	@Autowired
+	private AuthenticationManager manager;
+	
+	@Autowired
+	private JwtUtil jwtUtil;
+	
+	public String signup(User user) {
+		user.setPassword(encoder.encode(user.getPassword()));
+		repo.save(user);
+		return "User registered with name "+user.getUsername();
+	}
+	
+	public String signin(AuthDtoRequest authDtoRequest) {
+		manager.authenticate(new UsernamePasswordAuthenticationToken(authDtoRequest.getEmail(), authDtoRequest.getPassword()));
+		return jwtUtil.generateToken(user.loadUserByUsername(authDtoRequest.getEmail()));
+	}
+}
