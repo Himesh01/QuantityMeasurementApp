15b5721dbf9984660b59197a46cdbab14411eb8e
Himesh Kurmi <rajendrakurmi729@gmail.com>
2026-04-06 11:03:52 +0530

    [Himesh] : added SqlDatabase.java file

---
+++ src/main/java/com/apps/quantitymeasurement/repository/SqlDatabase.java
@@ -0,0 +1,11 @@
+package com.apps.quantitymeasurement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.apps.quantitymeasurement.model.User;
+
+@Repository
+public interface  SqlDatabase extends JpaRepository<User,Long> {
+     public  User findByEmail(String email);
+}
