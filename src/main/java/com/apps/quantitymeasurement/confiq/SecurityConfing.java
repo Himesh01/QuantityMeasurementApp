7d28890c2b6b0c44b879d5074741f6c61aa95750
Himesh Kurmi <rajendrakurmi729@gmail.com>
2026-04-06 10:39:38 +0530

    [Himesh] : added SecurityConfing file

--- src/main/java/com/apps/quantitymeasurement/confiq/SecurityConfing.java
+++ src/main/java/com/apps/quantitymeasurement/confiq/SecurityConfing.java
@@ -2,45 +2,79 @@ package com.apps.quantitymeasurement.confiq;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.apps.quantitymeasurement.security.JwtFilter;
+import com.apps.quantitymeasurement.security.UserDetail;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfing {
 	
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.cors(cors -> cors.configurationSource( corsConfigurationSource()))
-		.csrf(csrf -> csrf.disable())
-		.sessionManagement (session ->
-		session. sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-		.authorizeHttpRequests (authz -> authz
-				.anyRequest ().permitAll()
-				);
-
-		http.headers(headers -> headers. frameOptions(frame -> frame.disable() ));
-		return http.build();}
+	@Autowired
+	private JwtFilter jwtFilter;
+	
+	@Autowired
+	private UserDetail userDetail;
 	
 	@Bean
-	public UrlBasedCorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList( "http://localhost:3000", "http://localhost:8080")
-				);
-		configuration. setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-
-		configuration. setAllowedHeaders(Arrays.asList("*"));
-		configuration. setAllowCredentials(true);
-
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source. registerCorsConfiguration ( "/**", configuration);
-		return source;
+	 SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	return http.cors(cors -> cors.configurationSource( corsConfigurationSource()))
+	.csrf(csrf -> csrf.disable())
+	  .headers(headers -> headers.frameOptions(frame -> frame.disable()))
+	.sessionManagement (session ->
+	session. sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+	.authorizeHttpRequests (authz -> authz
+	.requestMatchers("/api/v1/auth/**").permitAll()
+	.requestMatchers("/h2-console/**").permitAll()
+	.requestMatchers("/swagger-ui/**").permitAll()
+	.anyRequest ().authenticated()
+	)
+	.addFilterBefore(jwtFilter,UsernamePasswordAuthenticationFilter.class)
+	.build();
 	}
+
+    @Bean
+    UrlBasedCorsConfigurationSource corsConfigurationSource() {
+	CorsConfiguration configuration = new CorsConfiguration();
+	configuration.setAllowedOrigins(Arrays.asList( "http://localhost:3000", "http://localhost:8080", "http://127.0.0.1:5500","http://localhost:4200")
+	);
+	configuration. setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+	configuration. setAllowedHeaders(Arrays.asList("*"));
+	configuration. setAllowCredentials(true);
+
+	UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	source. registerCorsConfiguration ( "/**", configuration);
+	return source;
+	}
+    
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+    	return config.getAuthenticationManager();
+    }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+    	return new BCryptPasswordEncoder();
+    }
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+	
 }
