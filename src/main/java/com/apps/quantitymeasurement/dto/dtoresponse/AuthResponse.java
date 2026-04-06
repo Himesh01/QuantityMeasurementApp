e6a15907312bcb4a34f38c47e549817d05dbd3f9
Himesh Kurmi <rajendrakurmi729@gmail.com>
2026-04-06 11:08:05 +0530

    [Himesh] : UC18 dtoresponse file

---
+++ src/main/java/com/apps/quantitymeasurement/dto/dtoresponse/AuthResponse.java
@@ -0,0 +1,14 @@
+package com.apps.quantitymeasurement.dto.dtoresponse;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+	private String token;
+	private String message;
+}
