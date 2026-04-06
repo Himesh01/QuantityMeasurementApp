a6cda626a75d241bcba7aba3f21d81ffc400bde7
Himesh Kurmi <rajendrakurmi729@gmail.com>
2026-04-06 11:07:30 +0530

    [Himesh] : UC18 Security file

---
+++ src/main/java/com/apps/quantitymeasurement/security/UserDetail.java
@@ -0,0 +1,30 @@
+package com.apps.quantitymeasurement.security;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.apps.quantitymeasurement.model.User;
+import com.apps.quantitymeasurement.repository.SqlDatabase;
+
+@Service
+public class UserDetail implements UserDetailsService {
+	
+	@Autowired
+	private SqlDatabase sqlDatabase;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = sqlDatabase.findByEmail(username);
+		
+		return org.springframework.security.core.userdetails.User
+				.builder()
+				.username(user.getEmail())
+				.password(user.getPassword())
+				.build();
+	}
+
+}
